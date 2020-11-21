0695024971b468c32af3c31dc0c1559c687201d4
Desislava Georgieva <desislava.georgieva@bbraun.com>
2020-11-21 13:32:23 +0200

    property binding

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,23 +6,32 @@ import { Component, OnInit } from '@angular/core';
     <p>
       The title is: {{ title }}
     </p>
+    <input [value]="generateTitle()">
+    <p>{{ generateTitle() }}</p>
+    <button>Save</button>
   `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
   title = 'Hello angular!';
 
-  newTitle = 'I love coding';
+  newTitle = 'Love coding';
 
   constructor() {
     this.changeTitle(this.newTitle);
   }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.title = 'This title is not the title, which you are looking for!';
+    }, 4000);
   }
 
   changeTitle(newTitle: string): void {
    this.title = newTitle;
   }
 
+  generateTitle(): string {
+    return 'This title was generate by title!';
+  }
 }
